6cdb9577d7a88ff3cb573d554c069665b7e5a46e
JenniaGo <jenniagorelik@gmail.com>
2022-11-21 15:42:45 +0200

    Create Install_Grafana.sh

---
+++ Scripts/Install_Grafana.sh
@@ -0,0 +1,4 @@
+#/bin/bash!
+sudo apt-get install -y adduser libfontconfig1
+wget https://dl.grafana.com/enterprise/release/grafana-enterprise_9.2.2_amd64.deb
+sudo dpkg -i grafana-enterprise_9.2.2_amd64.deb
