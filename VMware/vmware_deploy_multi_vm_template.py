40e84cabe874560c3e0ad4d0de68047220bd028d
JenniaGo <jenniagorelik@gmail.com>
2023-01-07 20:06:15 +0200

    Create vmware_deploy_multi_vm_template.py
    This code will connect to VMware Cloud on AWS and deploy multiple VMs from the specified template, using a specified name prefix and incrementing the number at the end of the name for each VM. The VMs will have the specified hardware specifications and will be deployed to the specified folder, resource pool, and datastore.

---
+++ VMware/vmware_deploy_multi_vm_template.py
@@ -0,0 +1,35 @@
+# Import the required libraries
+from vmware.vapi.vmc.client import create_vmc_client
+import vmware.vcenter.vm.template.deploy as vm_template_deploy
+import vmware.vcenter.vm.hardware.disk as vm_disk
+import vmware.vcenter.vm.hardware.nic as vm_nic
+
+# Connect to VMware Cloud on AWS using the VMC client
+client = create_vmc_client(refresh_token=REFRESH_TOKEN,
+                           org_id=ORG_ID,
+                           sddc_id=SDDC_ID)
+
+# Create the VM disk specification
+vm_disk_service = vm_disk.Disk(client)
+vm_disk_spec = vm_disk_service.create_spec(new_vmdk(size=10))
+
+# Create the VM NIC specification
+vm_nic_service = vm_nic.Nic(client)
+vm_nic_spec = vm_nic_service.create_spec(network=NETWORK_ID)
+
+# Deploy multiple VMs from the template
+vm_template_deploy_service = vm_template_deploy.TemplateDeploy(client)
+deploy_spec = vm_template_deploy_service.create_spec(name=VM_NAME_PREFIX,
+                                          folder=FOLDER_ID, 
+                                          resource_pool=RESOURCE_POOL_ID, 
+                                          datastore=DATASTORE_ID,
+                                          disks=[vm_disk_spec],
+                                          nics=[vm_nic_spec])
+for i in range(NUM_VMS):
+    # Update the name of the VM for each iteration
+    deploy_spec.name = f"{VM_NAME_PREFIX}-{i+1}"
+    
+    # Deploy the VM
+    deploy_task = vm_template_deploy_service.deploy(template_id=TEMPLATE_ID,
+                                          spec=deploy_spec)
+    client.vcenter.Task.wait_for_completion(deploy_task)
