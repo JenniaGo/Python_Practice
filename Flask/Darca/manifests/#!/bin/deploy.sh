82e646d9d4f83708471ed39b222e1423a9dcfd47
JenniaGo <jenniagorelik@gmail.com>
2023-01-09 01:35:03 +0200

    Delete Flask/Darca/manifests/#! directory

--- Flask/Darca/manifests/#!/bin/deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-kubectl apply -f ticket-submitter-deployment.yaml
-kubectl apply -f ticket-submitter-service.yaml
-kubectl apply -f mongodb-deployment.yaml
-kubectl apply -f mongodb-service.yaml
